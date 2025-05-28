8e1b876f0046d7babe0d605ac9a8278e49edde9f
karaman56 <166434324+karaman56@users.noreply.github.com>
2025-05-29 00:06:54 +0500

    подправил шаблон
    подправил шаблон

--- blog/views.py
+++ blog/views.py
@@ -36,11 +36,7 @@ def post_detail(request, slug):
 
 
 def contact(request):
-    """
-    Вьюхи не оптимизированы, потому что в последней задаче модуля Django ORM нужно их оптимизировать как раз на примере этого сайта.
-    """
-    # позже здесь будет код для статистики заходов на эту страницу
-    # и для записи фидбека
+
     folium_map = folium.Map(location=COMPANY_COORDINATES, zoom_start=12)
     folium.Marker(
         COMPANY_COORDINATES,
